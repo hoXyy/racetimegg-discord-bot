0af302dddef90eeb2ddd48c5589e0a260fb0374a
hoXyy <hoxtf2@outlook.com>
2020-05-13 09:02:19 +0200

    fix new races of the same game not being announced

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function NewRaceCheck() {
 		parsed.races.forEach(element => {
 			const race_name = (JSON.stringify(element.category.slug));
 			if (race_name.includes(config.filter) && !announced_races.includes(race_name)) {
-				announced_races.push(race_name);
+				announced_races.push(element.name);
 				race_channel.send('A new race is happening! Game: ' + element.category.name + ' - Category: ' + element.goal.name + ' - Link: https://racetime.gg' + element.url);
 			}
 		});
